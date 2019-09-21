4c093439013244da934063a3228802a2ab490f17
unknown <y343yu@edu.uwaterloo.ca>
2019-09-21 15:59:26 -0400

    Updates

---
+++ precache-manifest.d6eadd74171778669537635eef855847.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f26397a72f69be356fab199dce834d",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "d78157971c2f95e7b368",
+    "url": "/web/static/css/2.40d61c3f.chunk.css"
+  },
+  {
+    "revision": "a162b9728b2df2d9fd57",
+    "url": "/web/static/css/main.1a38294a.chunk.css"
+  },
+  {
+    "revision": "d78157971c2f95e7b368",
+    "url": "/web/static/js/2.06ee9be0.chunk.js"
+  },
+  {
+    "revision": "a162b9728b2df2d9fd57",
+    "url": "/web/static/js/main.41e8d06e.chunk.js"
+  },
+  {
+    "revision": "70c22b3514e757e782ce",
+    "url": "/web/static/js/runtime~main.47739002.js"
+  },
+  {
+    "revision": "303e53a989315747ecba86af7dab6f0a",
+    "url": "/web/static/media/WLU.303e53a9.png"
+  },
+  {
+    "revision": "a15ecbc3e019769d57b3cb7c2b0f6d81",
+    "url": "/web/static/media/background.a15ecbc3.jpg"
+  },
+  {
+    "revision": "77fa3b887a41ccc48cab8573307291bf",
+    "url": "/web/static/media/biquadris.77fa3b88.png"
+  },
+  {
+    "revision": "e259d3f767aadf0636a763181e17f4dd",
+    "url": "/web/static/media/cueCards.e259d3f7.jpg"
+  },
+  {
+    "revision": "fdaaf92a4a69042d9544b47168df1a62",
+    "url": "/web/static/media/logo.fdaaf92a.png"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/web/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/web/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/web/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/web/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f8c6087df9e2e1d799924f665c18af03",
+    "url": "/web/static/media/scotia.f8c6087d.png"
+  },
+  {
+    "revision": "a4f4d6bd6320a59cc296581e77894989",
+    "url": "/web/static/media/siteCVT.a4f4d6bd.png"
+  },
+  {
+    "revision": "95d97bb6203d079ca067e14475057e66",
+    "url": "/web/static/media/uw.95d97bb6.png"
+  },
+  {
+    "revision": "e5433615b9d0b13adaa8ec0a3becf033",
+    "url": "/web/static/media/work.e5433615.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.769eb2de46f88d1fb6fadc5a893316e2.js"
+  "/web/precache-manifest.d6eadd74171778669537635eef855847.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
